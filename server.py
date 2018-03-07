b70b4767b78d4bdd36e1c056938a68c3db8faf0a
Imran Ahmed <iahmed@mail.umw.edu>
2018-03-07 07:22:08 +0000

    login screen is up

--- server.py
+++ server.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return render_template('index.html')
-
+    return render_template('home.html')
+    
+@app.route('/login')
+def loginPage():
+    return render_template('login.html')
+    
 # start the server
 if __name__ == '__main__':
      app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
